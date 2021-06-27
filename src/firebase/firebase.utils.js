da3acb67bb78cf94fc989e7ee735ed11725fa61a
Parth Shah <parth.shah1492@gmail.com>
2021-06-27 04:43:01 -0400

    Complete shopping app with checkout app and checkout dropdown functionality with REDUX and reselect

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,15 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyBn1mZsKtdqtFdCEzLnCamEFOFo9sRoQhs",
-  authDomain: "crown-db-dae99.firebaseapp.com",
-  projectId: "crown-db-dae99",
-  storageBucket: "crown-db-dae99.appspot.com",
-  messagingSenderId: "1015283997190",
-  appId: "1:1015283997190:web:4e4c4db8e5af2a075ac187",
-  measurementId: "G-MKZ70DBCVM",
+  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+  authDomain: 'crwn-db.firebaseapp.com',
+  databaseURL: 'https://crwn-db.firebaseio.com',
+  projectId: 'crwn-db',
+  storageBucket: 'crwn-db.appspot.com',
+  messagingSenderId: '850995411664',
+  appId: '1:850995411664:web:7ddc01d597846f65'
 };
 
 firebase.initializeApp(config);
@@ -29,10 +29,10 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         displayName,
         email,
         createdAt,
-        ...additionalData,
+        ...additionalData
       });
     } catch (error) {
-      console.log("error creating user", error.message);
+      console.log('error creating user', error.message);
     }
   }
 
@@ -43,7 +43,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
